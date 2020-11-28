3e835c63ac2ed6b1390ee7eac478d468da8a688b
gowoonsori <gowoonsori97@naver.com>
2020-11-29 00:47:11 +0900

    add : event domain

---
+++ src/test/java/me/gowoo/studyrestapi/events/EventTest.java
@@ -0,0 +1,30 @@
+package me.gowoo.studyrestapi.events;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("의성")
+                .description("안녕하세요")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        //given
+        String name ="Event";
+        String description = "Spring";
+        //when
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+        //then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
