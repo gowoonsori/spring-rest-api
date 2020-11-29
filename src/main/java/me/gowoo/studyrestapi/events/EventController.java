54b2186453a39440f49cc158cd05dd1052d0975d
gowoonsori <gowoonsori97@naver.com>
2020-11-30 00:11:07 +0900

    add : controller test code(mockMVC)

---
+++ src/main/java/me/gowoo/studyrestapi/events/EventController.java
@@ -0,0 +1,23 @@
+package me.gowoo.studyrestapi.events;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = "application/hal+json; charset=UTF-8")
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event){
+        URI createdUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(createdUri).body(event);
+    }
+}
---
+++ src/test/java/me/gowoo/studyrestapi/events/EventControllerTests.java
@@ -0,0 +1,49 @@
+package me.gowoo.studyrestapi.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class EventControllerTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void creatEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("rest api practice")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,11,29,17,24))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,11,29,17,33))
+                .beginEventDateTime(LocalDateTime.of(2020,11,29,17,53))
+                .endEventDateTime(LocalDateTime.of(2020,11,29,17,01))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
+    }
+}
