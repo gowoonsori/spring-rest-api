545b1e4c9d1fe611f823b69cc2ebcf40d6df00f9
gowoonsori <gowoonsori97@naver.com>
2020-12-07 15:35:48 +0900

    Add Validator구현과 사용, test Description(DisplayName)사용

--- src/main/java/me/gowoo/studyrestapi/events/EventController.java
+++ src/main/java/me/gowoo/studyrestapi/events/EventController.java
@@ -3,10 +3,12 @@ package me.gowoo.studyrestapi.events;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -19,19 +21,34 @@ public class EventController {
 
     private final ModelMapper modelMapper;
 
-    public EventController(EventRepository eventRepository,ModelMapper modelMapper) {
+    private final EventValidator eventValidater;
+
+    public EventController(EventRepository eventRepository,ModelMapper modelMapper,EventValidator eventValidater) {
         this.eventRepository = eventRepository;
         this.modelMapper = modelMapper;
+        this.eventValidater = eventValidater;
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody EventDto eventDto){
+    public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
 //        Event event = Event.builder()
 //                .name(eventDto.getName())
 //                .description(eventDto.getDescription())
 //                ..
 //          위 와 같은 Dto를 Entity에 맵핑해줘야 하는데 너무 귀찮다... 따라서 ModelMapper를 사용
 //          Reflaction이 발생해 직접 맵핑하는 것보단 속도가 느려질 수 있다.
+
+        //Bean Validator 이용
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        //Custom Validator이용
+        eventValidater.validate(eventDto,errors);
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
         Event event = modelMapper.map(eventDto,Event.class);
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
--- src/test/java/me/gowoo/studyrestapi/events/EventControllerTests.java
+++ src/test/java/me/gowoo/studyrestapi/events/EventControllerTests.java
@@ -2,11 +2,11 @@ package me.gowoo.studyrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -28,19 +28,17 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("기본 객체 생성시 성공")
     public void creatEvent() throws Exception {
-        Event event = Event.builder()
-                .id(100)
+        EventDto event = EventDto.builder()
                 .name("Spring")
                 .description("rest api practice")
-                .beginEnrollmentDateTime(LocalDateTime.of(2020,11,29,17,24))
-                .closeEnrollmentDateTime(LocalDateTime.of(2020,11,29,17,33))
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,11,27,17,24))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,11,28,17,33))
                 .beginEventDateTime(LocalDateTime.of(2020,11,29,17,53))
-                .endEventDateTime(LocalDateTime.of(2020,11,29,17,01))
+                .endEventDateTime(LocalDateTime.of(2020,11,30,17,01))
                 .basePrice(100)
                 .maxPrice(200)
-                .free(true)
-                .offline(true)
                 .limitOfEnrollment(100)
                 .location("강남역")
                 .build();
@@ -61,5 +59,75 @@ public class EventControllerTests {
                 .andExpect(jsonPath("id").value(Matchers.not(100))) //id,free..와 같은 필드는 입력할 수 없는 값이어야한다.
                 .andExpect(jsonPath("free").value(Matchers.not(true))) //id,free..와 같은 필드는 입력할 수 없는 값이어야한다.
         ;
+    }
+
+    @Test
+    @DisplayName("접근 불가능한 id,price 값 지정하는 경우 에러발생")
+    public void creatEventBadRequest() throws Exception {
+        Event event = Event.builder()
+                .id(100)
+                .name("Spring")
+                .description("rest api practice")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,11,27,17,24))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,11,28,17,33))
+                .beginEventDateTime(LocalDateTime.of(2020,11,29,17,53))
+                .endEventDateTime(LocalDateTime.of(2020,11,30,17,01))
+                .basePrice(100)
+                .maxPrice(200)
+                .free(true)
+                .offline(true)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        /*mock 객체는 null을 return하기 때문에  contoller에서
+        * null에 save를 수행해 null Exception이 발생한다. save가 수행할때 event가 들어가게 mocking을 해주자*/
+        //Mockito.when(eventRepository.save(event)).thenReturn(event); (@MockBean, @MockMvc)
+
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("필수 값이 없는 객체 생성할때 에러발생")
+    public void 이벤트생성_bad_Request_값이_없을때() throws Exception{
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(this.objectMapper.writeValueAsString(eventDto)))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("입력값의 잘못된 경우(조건값이 다른 경우) 에러 발생")
+    public void creatEventBadRequest_wrong_input() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("rest api practice")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020,11,29,17,24))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020,11,29,17,33))
+                .beginEventDateTime(LocalDateTime.of(2020,11,29,17,53))
+                .endEventDateTime(LocalDateTime.of(2020,11,20,17,01))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        /*mock 객체는 null을 return하기 때문에  contoller에서
+         * null에 save를 수행해 null Exception이 발생한다. save가 수행할때 event가 들어가게 mocking을 해주자*/
+        //Mockito.when(eventRepository.save(event)).thenReturn(event); (@MockBean, @MockMvc)
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 }
