d55c1f731fbf9de6f6dd37164f37944380cea6e5
gowoonsori <gowoonsori97@naver.com>
2020-12-20 16:58:45 +0900

    add : 기본인증 security 설정

--- src/main/java/me/gowoo/studyrestapi/StudyRestApiApplication.java
+++ src/main/java/me/gowoo/studyrestapi/StudyRestApiApplication.java
@@ -1,9 +1,7 @@
 package me.gowoo.studyrestapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
@@ -11,12 +9,6 @@ import org.springframework.context.annotation.PropertySource;
 public class StudyRestApiApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(StudyRestApiApplication.class, args);
     }
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
 }
--- src/test/java/me/gowoo/studyrestapi/accounts/AccountServiceTest.java
+++ src/test/java/me/gowoo/studyrestapi/accounts/AccountServiceTest.java
@@ -1,11 +1,16 @@
 package me.gowoo.studyrestapi.accounts;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,10 +22,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("Test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Autowired
     AccountService accountService;
+
     @Autowired
-    AccountRepository accountRepository;
+    PasswordEncoder passwordEncoder;
+
     @Test
     public void findByEmail(){
         //given
@@ -31,12 +42,24 @@ public class AccountServiceTest {
                 .password(password)
                 .roles(Set.of(AccountRole.ADMIN,AccountRole.USER))
                 .build();
-        this.accountRepository.save(account);
+        this.accountService.saveAccount(account);
         //When
         UserDetailsService userDetailsService = (UserDetailsService)accountService;
         UserDetails userDetails = userDetailsService.loadUserByUsername(email);
 
         //Then
-        assertThat(userDetails.getPassword()).isEqualTo(password);
+        assertThat(this.passwordEncoder.matches(password, userDetails.getPassword())).isTrue();
+    }
+
+    @Test
+    public void findByUserEmailFail(){
+        //Expected
+        String username = "1234@naver.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.contains(username));
+
+        //when
+        accountService.loadUserByUsername(username);
+
     }
 }
